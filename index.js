218e3af2cd556195881ce79d85e0d38e17da47c6
olamilekan29 <farouqolamilekan2006@gmail.com>
2023-07-01 10:23:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// //select element with the "svg" tag and store them in a Nodelist called "stars"
+// const stars = document.querySelectorAll(".stars svg");
+
+// // loop through the "stars" Nodelist
+// stars.forEach(stars, index1 => {
+//     // Add an event listner that runs a function when the "click" event is triggered
+//     star.addEventlistener("click",() => ){
+//         // loop through the "stars" Nodelist Again
+//         star.forEach((star, index2) =>{
+//             // Add the "active" class to the clickad star and any star with a lower index
+//             // and remove the "active" class from any star with higher index
+//         }
+//         )
+//      index1 >= index2 ? star.classList.add("active") :  star.classList.remove("active")  
+//     }
+// });
