8f27276e637b4dc568381609a014cde58f33a6ae
olamilekan29 <farouqolamilekan2006@gmail.com>
2023-07-09 16:57:51 +0100

    logic done

--- index.js
+++ index.js
@@ -14,3 +14,62 @@
 //      index1 >= index2 ? star.classList.add("active") :  star.classList.remove("active")  
 //     }
 // });
+
+
+let button1 = document.getElementById("one")
+let button2 = document.getElementById("two")
+let button3 = document.getElementById("three")
+let button4 = document.getElementById("four")
+let button5 = document.getElementById("five")
+
+console.log(button1.innerHTML)
+button1.addEventListener("click", () => {
+    console.log("Button One Clicked")
+    button1.classList.add("active")
+    button2.classList.remove("active")
+    button3.classList.remove("active")
+    button4.classList.remove("active")
+    button5.classList.remove("active")
+    localStorage.setItem("rate", button1.innerHTML)
+
+})
+
+button2.addEventListener("click", () => {
+    console.log("Button Two Clicked")
+    button1.classList.remove("active")
+    button2.classList.add("active")
+    button3.classList.remove("active")
+    button4.classList.remove("active")
+    button5.classList.remove("active")
+    localStorage.setItem("rate", button2.innerHTML)
+})
+
+button3.addEventListener("click", () => {
+    console.log("Button Three Clicked")
+    button1.classList.remove("active")
+    button2.classList.remove("active")
+    button3.classList.add("active")
+    button4.classList.remove("active")
+    button5.classList.remove("active")
+    localStorage.setItem("rate", button3.innerHTML)
+})
+
+button4.addEventListener("click", () => {
+    console.log("Button Four Clicked")
+    button1.classList.remove("active")
+    button2.classList.remove("active")
+    button3.classList.remove("active")
+    button4.classList.add("active")
+    button5.classList.remove("active")
+    localStorage.setItem("rate", button4.innerHTML)
+})
+
+button5.addEventListener("click", () => {
+    console.log("Button Five Clicked")
+    button1.classList.remove("active")
+    button2.classList.remove("active")
+    button3.classList.remove("active")
+    button4.classList.remove("active")
+    button5.classList.add("active")
+    localStorage.setItem("rate", button5.innerHTML)
+})
